57a2c2a0aab0c8f17d87e85156e4c8493db8abea
nataliohoteles-crypto <227802105+Nataliogc@users.noreply.github.com>
2026-03-27 16:04:27 +0100

    srsdfsgdf

--- comments.js
+++ comments.js
@@ -1,1 +1,12 @@
-const comments_data = [];
+const comments_data = [
+  {
+    "id": 1774623498855,
+    "pId": "5.3",
+    "pTitle": "Protocolo de Registro – Grupos de Menores",
+    "author": "natalio",
+    "text": "hola",
+    "date": "2026-03-27T14:58:18.855Z",
+    "status": "approved",
+    "reply": null
+  }
+];
